30a0b4cad47940acbd6b0804e64f3a10fdea4e65
Rakibul Islam <81356942+iamrakib097@users.noreply.github.com>
2024-09-04 02:10:12 +0600

    style added

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,19 +1,19 @@
-import { useLoaderData } from "react-router-dom";
-import { getMenu } from "../../services/apiRestaurant";
-import MenuItem from "../menu/MenuItem.jsx";
+import { useLoaderData } from 'react-router-dom'
+import { getMenu } from '../../services/apiRestaurant'
+import MenuItem from '../menu/MenuItem.jsx'
 function Menu() {
-  const menu = useLoaderData();
+  const menu = useLoaderData()
   return (
-    <ul>
+    <ul className='divide-y divide-stone-200 px-2'>
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
     </ul>
-  );
+  )
 }
 
 export async function loader() {
-  const menu = await getMenu();
-  return menu;
+  const menu = await getMenu()
+  return menu
 }
-export default Menu;
+export default Menu
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,21 +1,22 @@
-import Header from "./Header";
-import CartOverView from "../features/cart/CartOverview.jsx";
-import { Outlet, useNavigation } from "react-router-dom";
-import Loader from "./Loader.jsx";
+import Header from './Header'
+import CartOverView from '../features/cart/CartOverview.jsx'
+import { Outlet, useNavigation } from 'react-router-dom'
+import Loader from './Loader.jsx'
 const AppLayout = () => {
-  const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  const navigation = useNavigation()
+  const isLoading = navigation.state === 'loading'
   return (
-    <div className="layout">
+    <div className='grid h-screen grid-rows-[auto_1fr_auto]'>
       {isLoading && <Loader />}
       <Header />
-
-      <main>
-        <Outlet />
-      </main>
+      <div className='overflow-scroll'>
+        <main className='mx-auto max-w-3xl'>
+          <Outlet />
+        </main>
+      </div>
       <CartOverView />
     </div>
-  );
-};
+  )
+}
 
-export default AppLayout;
+export default AppLayout
